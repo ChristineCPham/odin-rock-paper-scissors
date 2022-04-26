56ecf7440570dc3d1284011b6ad917882b631962
ChristineCPham <christinepham44@gmail.com>
2022-04-26 13:18:05 -0700

    Created one game round
    playRound() function plays one round of the game.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let playerSelection;
 let computerSelection;
+let playerResult;
 
 function userPrompt() {
     playerSelection = prompt('Enter rock, paper, or scissors.');
+
+    //Check if user entered a response
+    (playerSelection) ? true: alert('This round has been cancelled.')
 
     // Make playerSelection case-insensitive
     playerSelection = playerSelection.slice(0,1).toUpperCase()
@@ -14,12 +18,11 @@ function userPrompt() {
 
 // Check if the user entered Rock, Paper, or Scissors
 function checkUserPrompt() {
-    if (playerSelection === 'Rock' || playerSelection === 'Paper' || playerSelection === 'Paper') {
+    if (playerSelection === 'Rock' || playerSelection === 'Paper' || playerSelection === 'Scissors') {
     } else {
         alert('Sorry. That is not a valid response.');
     }
 }
-computerPlay();
 function computerPlay() {
     //Generate random number
     let randomNumber;
@@ -39,3 +42,36 @@ function computerPlay() {
     //Remove when done
     console.log(computerSelection);
 }
+
+function playRound() {
+    userPrompt();
+    checkUserPrompt();
+    computerPlay();
+
+    //Determine winner of a round
+    if (playerSelection === 'Rock' && computerSelection === 'Rock') {
+        playerResult = 'tie';
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        playerResult = 'lose';
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        playerResult = 'win';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        playerResult = 'win';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
+        playerResult = 'tie';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        playerResult = 'lose';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        playerResult = 'lose';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        playerResult = 'win';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
+        playerResult = 'tie';
+    } else {
+        alert('Error at function playRound().')
+    }
+
+    //Delete when done
+    console.log(playerResult);
+}    
+
